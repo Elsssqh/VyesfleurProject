df5896293967d6c42de2ce1bb1137b643f242c4a
Elsssqh <Elss.hqh@gmail.com>
2024-06-27 16:57:22 +0700

    HomePage

---
+++ resources/views/home/footer.blade.php
@@ -0,0 +1,91 @@
+<section class="info_section  layout_padding2-top">
+    <div class="social_container">
+      <div class="social_box">
+        <a href="">
+          <i class="fa fa-facebook" aria-hidden="true"></i>
+        </a>
+        <a href="">
+          <i class="fa fa-twitter" aria-hidden="true"></i>
+        </a>
+        <a href="https://www.instagram.com/vyes.fleur/">
+          <i class="fa fa-instagram" aria-hidden="true"></i>
+        </a>
+        <a href="">
+          <i class="fa fa-youtube" aria-hidden="true"></i>
+        </a>
+      </div>
+    </div>
+    <div class="info_container ">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-lg-3">
+            <h6>
+              ABOUT US
+            </h6>
+            <p>
+            With a commitment to craftsmanship and a passion for beauty. Whether you're marking a milestone or simply celebrating the everyday, Vyes Fleur is your go-to destination for stunning florals that leave a lasting impression.
+            </p>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <div class="info_form ">
+              <h5>
+                Newsletter
+              </h5>
+              <form action="#">
+                <input type="email" placeholder="Enter your email">
+                <button>
+                  Subscribe
+                </button>
+              </form>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <h6>
+              NEED HELP
+            </h6>
+            <p>
+            At Vyes Fleur, we're dedicated to helping you find the perfect floral arrangements for any occasion. Whether you need assistance choosing the ideal bouquet to express your sentiments or want advice on floral design, our team is here to help.
+            </p>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <h6>
+              CONTACT US
+            </h6>
+            <div class="info_link-box">
+              <a href="">
+                <i class="fa fa-map-marker" aria-hidden="true"></i>
+                <span> Anggrek Mas 1 Blk. K No.25, Taman Baloi, Batam Kota, Batam City, Riau Islands 29444s </span>
+              </a>
+              <a href="">
+                <i class="fa fa-phone" aria-hidden="true"></i>
+                <span> 0898-1910-300</span>
+              </a>
+              <a href="">
+                <i class="fa fa-envelope" aria-hidden="true"></i>
+                <span> vyesfleur@gmail.com</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- footer section -->
+    <footer class=" footer_section">
+      <div class="container">
+        <p>
+          &copy; <span id="displayYear"></span> All Rights Reserved By Vyes Fleur
+        </p>
+      </div>
+    </footer>
+    <!-- footer section -->
+
+  </section>
+
+  <!-- end info section -->
+
+
+  <script src="{{asset('js/jquery-3.4.1.min.js')}}"></script>
+  <script src="{{asset('js/bootstrap.js')}}"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
+  </script>
+  <script src="{{asset('js/custom.js')}}"></script>
---
+++ resources/views/home/slider.blade.php
@@ -0,0 +1,33 @@
+ <section class="slider_section">
+      <div class="slider_container">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <div class="container-fluid">
+                <div class="row">
+                  <div class="col-md-7">
+                    <div class="detail-box"> 
+                      <h1>
+                        Welcome To  <br>
+                        Vyes Fleur
+                      </h1>
+                      <p>
+                      Welcome to Vyes Fleur! Discover elegant floral arrangements crafted with passion and dedication. Whether it's for a special occasion or to brighten your space, let us bring your floral visions to life.
+                      </p>
+                       <a class="btn btn-primary" href="{{url('contact')}}">contact us</a>
+                    </div>
+                  </div>
+                  <div class="col-md-5 ">
+                    <div class="img-box">
+                      <img style="width:530px" src="images/pic.jpg" alt="" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+          </div>
+          
+        </div>
+      </div>
+    </section>
